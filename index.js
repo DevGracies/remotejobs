6eb07d44e94d5e333689bc43c8df7e9e4a3910f0
Devgraices <graceadegunle16@gmail.com>
2025-02-19 15:36:59 +0100

    commit

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import express from "express";
 import "dotenv/config";
 import cron from "node-cron";
-
+import fs from "fs";
+import path from "path";
 import { fetchJobListings, sendToTelex } from "./job.js";
 
 const app = express();
 app.use(express.json());
+
+app.get("/integration.json", (req, res) => {
+  const filePath = path.join(process.cwd(), "integration.json");
+  fs.readFile(filePath, "utf8", (error, data) => {
+    if (error) {
+      return res.status(500).json({ error: "cant' read read" });
+    }
+    try {
+      const json = JSON.parse(data);
+      res.status(200).json(json);
+    } catch (error) {
+      res.status(500).json({ error: "error from json" });
+    }
+  });
+});
 
 cron.schedule("0 */3 * * *", async () => {
   try {
